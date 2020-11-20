d2511649adb45c0bb9cb9ec7d3ecc3d6d62bf46d
Vlad <2beats4hits@gmail.com>
2020-11-20 02:56:09 +0200

    Vector: Add filter menu option into ActionBar

--- app/src/main/java/me/vladsmirnov/cocktailapp/MainActivity.kt
+++ app/src/main/java/me/vladsmirnov/cocktailapp/MainActivity.kt
@@ -1,11 +1,28 @@
 package me.vladsmirnov.cocktailapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+
+        return super.onOptionsItemSelected(item)
+
+    }
+
 }
