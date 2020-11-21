c9217547929097c796b24ffbb4eb2e36a822c55a
Vlad <2beats4hits@gmail.com>
2020-11-21 23:22:43 +0200

    Misc: Kotlin extensions

---
+++ app/src/main/java/me/vladsmirnov/cocktailapp/utils/Extensions.kt
@@ -0,0 +1,7 @@
+package me.vladsmirnov.cocktailapp.utils
+
+import android.content.Context
+import android.widget.Toast
+
+fun Context.toast(message: CharSequence) =
+    Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
