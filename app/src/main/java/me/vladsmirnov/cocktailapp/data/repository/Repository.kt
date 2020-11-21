b529fcce2aedb99698abb3097d520f43a3bf64a4
Vlad <2beats4hits@gmail.com>
2020-11-21 23:08:59 +0200

    REST: Add "CocktailsFIlter" query to repository

--- app/src/main/java/me/vladsmirnov/cocktailapp/data/repository/Repository.kt
+++ app/src/main/java/me/vladsmirnov/cocktailapp/data/repository/Repository.kt
@@ -3,8 +3,17 @@ package me.vladsmirnov.cocktailapp.data.repository
 import me.vladsmirnov.cocktailapp.data.models.NetworkResult
 import me.vladsmirnov.cocktailapp.data.network.CocktailNetworkService
 import me.vladsmirnov.cocktailapp.data.network.response_types.CocktailResponse
+import me.vladsmirnov.cocktailapp.data.network.response_types.CocktailsFilterResponse
 
 class Repository(private val service: CocktailNetworkService) {
+
+    suspend fun getCocktailFilters(): CocktailsFilterResponse {
+        return when (val result = service.getCocktailFiltersList()) {
+            is NetworkResult.Success -> result.data
+            is NetworkResult.Error -> throw result.error
+        }
+
+    }
 
     suspend fun getCocktailsByCategory(categoryName: String): CocktailResponse {
         return when (val result = service.getCocktailsByCategory(categoryName)) {
