315eefece0592242ca4bfa7e5ba2f7cc65c12bb8
Vlad <2beats4hits@gmail.com>
2020-11-21 06:30:26 +0200

    REST: Define "NetworkResult" class

---
+++ app/src/main/java/me/vladsmirnov/cocktailapp/data/models/NetworkResult.kt
@@ -0,0 +1,6 @@
+package me.vladsmirnov.cocktailapp.data.models
+
+sealed class NetworkResult<out T : Any> {
+    data class Success<out T : Any>(val data: T) : NetworkResult<T>()
+    data class Error(val error: Exception) : NetworkResult<Nothing>()
+}
