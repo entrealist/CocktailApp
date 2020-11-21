97b493e03fd081f03fc4ea241d5e13bdd393b226
Vlad <2beats4hits@gmail.com>
2020-11-21 23:05:54 +0200

    Data: Create separator model

--- app/src/main/java/me/vladsmirnov/cocktailapp/data/models/Cocktail.kt
+++ app/src/main/java/me/vladsmirnov/cocktailapp/data/models/Cocktail.kt
@@ -5,9 +5,17 @@ import com.squareup.moshi.Json
 data class Cocktail(
     @Json(name = "idDrink") val id: Int,
     @Json(name = "strDrink") val title: String,
-    @Json(name = "strDrinkThumb") val thumbUrl: String)
+    @Json(name = "strDrinkThumb") val thumbUrl: String,
+)
+
+data class CocktailCategorized(
+    val id: Int,
+    val title: String,
+    val thumbUrl: String,
+    var category: String
+)
 
 sealed class CocktailModel {
-    data class CocktailItem(val cocktail: Cocktail) : CocktailModel()
+    data class CocktailItem(val cocktail: CocktailCategorized) : CocktailModel()
     data class CategoryItem(val title: String) : CocktailModel()
 }
