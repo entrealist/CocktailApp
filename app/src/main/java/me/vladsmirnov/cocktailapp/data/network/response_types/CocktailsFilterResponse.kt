76ee2d9741135b7acafb920d4b8aa4ac9a1e06ac
Vlad <2beats4hits@gmail.com>
2020-11-21 06:21:57 +0200

    REST: Define "CocktailFilter" response

---
+++ app/src/main/java/me/vladsmirnov/cocktailapp/data/network/response_types/CocktailsFilterResponse.kt
@@ -0,0 +1,9 @@
+package me.vladsmirnov.cocktailapp.data.network.response_types
+
+import com.squareup.moshi.Json
+import me.vladsmirnov.cocktailapp.data.models.CocktailFilter
+
+class CocktailsFilterResponse  {
+    @Json(name = "drinks")
+    val filters: List<CocktailFilter>? = null
+}
